4ae113208bf9d9adbf40e556a61c670f03962371
Xiphe <github@xiphe.net>
2015-09-17 00:47:51 +0200

    feat: rework rules api

--- spec/mark-obsolete-spec.js
+++ spec/mark-obsolete-spec.js
@@ -2,97 +2,70 @@ describe('mark-obsolete', function() {
   'use strict';
 
   var markObsolete = require('../lib/mark-obsolete');
-  var cleanupBackups = require('../lib/index.js');
+  var constants = require('../lib/constants');
 
-  it('should mark objects when to many in range', function() {
-    var files = [
-      {age: 0},
-      {age: 50},
-      {age: 700}
-    ];
-    var rules = [{
-      age: 1000,
-      keep: 2
-    }];
-    markObsolete(files, rules);
-    expect(files[0].obsolete).toBe(true);
-    expect(files[1].obsolete).toBeUndefined();
-    expect(files[2].obsolete).toBeUndefined();
-  });
-
-  it('should allow -1 to keep all', function() {
-    var files = [
-      {age: 0},
-      {age: 50},
-      {age: 700}
-    ];
-    var rules = [{
-      age: 1000,
-      keep: -1
-    }];
-    markObsolete(files, rules);
-    expect(files[0].obsolete).toBeUndefined();
-    expect(files[1].obsolete).toBeUndefined();
-    expect(files[2].obsolete).toBeUndefined();
-  });
-
-  it('should not mark objects out of rule bounds', function() {
-    var files = [
-      {age: 0},
-      {age: 50},
-      {age: 700}
-    ];
-    var rules = [{
-      age: 100,
-      keep: 2
-    }];
-    markObsolete(files, rules);
-    expect(files[0].obsolete).toBeUndefined();
-  });
-
-  it('should not mark objects handled by previous rules', function() {
-    var files = [
-      {age: 0},
-      {age: 20},
-      {age: 50},
-      {age: 100},
-      {age: 700},
-      {age: 1000},
-      {age: 1001}
-    ];
-    var rules = [{
-      age: 100,
-      keep: 2
-    }, {
-      age: 1000,
-      keep: 1
-    }];
-    markObsolete(files, rules);
-    expect(files[0].obsolete).toBe(true);
-    expect(files[1].obsolete).toBe(true);
-    expect(files[2].obsolete).toBeUndefined();
-    expect(files[3].obsolete).toBeUndefined();
-    expect(files[4].obsolete).toBe(true);
-    expect(files[5].obsolete).toBeUndefined();
-    expect(files[6].obsolete).toBeUndefined();
-  });
-
-  it('should throw when a range is smaller than the previous', function() {
-    var files = [
-      {age: 0},
-      {age: 50},
-      {age: 700}
-    ];
-    var rules = [{
-      age: 100,
-      keep: 2
-    }, {
-      age: 40,
-      keep: 0
-    }];
-
+  it('should throw if a rule is 0', function() {
+    var files = [{age: 5}];
+    var rules = [0];
     expect(function() {
       markObsolete(files, rules);
-    }).toThrow(new Error(cleanupBackups.CONST.ERR.AGE_DID_NOT_INCREASE));
+    }).toThrow(new Error(constants.ERR.ZERO_IS_NOT_A_VALID_RULE));
+  });
+
+  it('should mark files obsolete that do not match interval', function() {
+    var files = [];
+    for (var i = 0; i < 12; i += 1) {
+      files.push({age: i});
+    }
+    var rules = [5];
+    markObsolete(files, rules);
+    expect(files[0].obsolete).toBeUndefined();
+    expect(files[1].obsolete).toBe(true);
+    expect(files[2].obsolete).toBe(true);
+    expect(files[3].obsolete).toBe(true);
+    expect(files[4].obsolete).toBe(true);
+    expect(files[5].obsolete).toBeUndefined();
+    expect(files[6].obsolete).toBe(true);
+    expect(files[9].obsolete).toBe(true);
+    expect(files[10].obsolete).toBeUndefined();
+    expect(files[11].obsolete).toBe(true);
+  });
+
+  it('should work with multiple intervals', function() {
+    var files = [];
+    for (var i = 0; i < 12; i += 1) {
+      files.push({age: i});
+    }
+    var rules = [2, 5];
+    markObsolete(files, rules);
+    expect(files[0].obsolete).toBeUndefined();
+    expect(files[1].obsolete).toBe(true);
+    expect(files[2].obsolete).toBeUndefined(true);
+    expect(files[3].obsolete).toBe(true);
+    expect(files[4].obsolete).toBeUndefined();
+    expect(files[5].obsolete).toBeUndefined();
+    expect(files[6].obsolete).toBe(true);
+    expect(files[9].obsolete).toBe(true);
+    expect(files[10].obsolete).toBeUndefined();
+    expect(files[11].obsolete).toBe(true);
+  });
+
+  it('should work with complex intervals', function() {
+    var files = [];
+    for (var i = 0; i < 2600; i += 1) {
+      files.push({age: i});
+    }
+    var rules = [
+      2, // fake hour
+      2 * 5, // fake day
+      2 * 5 * 7, // fake week
+      2 * 5 * 3, // fake month-ish
+      2 * 5 * 65 // fake year
+    ];
+    markObsolete(files, rules);
+    var filteredFiles = files.filter(function(file) {
+      return !file.obsolete;
+    });
+    expect(filteredFiles.length).toBe(21);
   });
 });
